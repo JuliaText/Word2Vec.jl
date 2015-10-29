b4d202d0801f7cd3875324a2db10504a898b1ecc
Weijian Zhang <weijian.z@hotmail.com>
2015-10-29 13:40:55 +0000

    fix test error

--- test/load.jl
+++ test/load.jl
@@ -1,6 +1,7 @@
 big = joinpath(Pkg.dir("Word2Vec"), "data", "big.txt")
 word2vec(big, "bigvecs.txt")
 model = wordvectors("bigvecs.txt") 
+rm("bigvecs.txt")
 words = vocabulary(model)
 
 word = words[rand(1:end)]
@@ -16,10 +17,8 @@ s = similarity(model, word, word2)
 
 csw = cosine_similar_words(model, word, n)
 
-word3 = word3[rand(1:end)]
+word3 = words[rand(1:end)]
 
-inx, mes = analogy(model, [word1, word2], [word3])
-
-rm("bigvecs.txt")
+inx, mes = analogy(model, [word, word2], [word3])
 
 println(":)")
